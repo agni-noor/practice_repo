b9204a08aa62a0924c78b5df05be1f9e1cf90c48
agni-noor <agniotulonio.noor@optimizely.com>
2024-12-16 22:05:37 +0600

    array

---
+++ TS/array.js
@@ -0,0 +1,4 @@
+var fruits = [];
+var heroes = [];
+heroes.push("spiderman");
+console.log(heroes);
---
+++ TS/array.ts
@@ -0,0 +1,13 @@
+const fruits:string[] = [];
+const heroes: Array<string> = [] 
+heroes.push("spiderman")
+console.log(heroes)
+
+type User = {
+    name:string,
+    email:string
+}
+
+const allUser:User[] = []
+
+export{}
