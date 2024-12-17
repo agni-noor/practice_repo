5e5505f872e654aba2b270646e70b1341178dc03
agni-noor <agniotulonio.noor@optimizely.com>
2024-12-17 11:00:02 +0600

    enums

---
+++ TS/enum.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var SEATCHOICE;
+(function (SEATCHOICE) {
+    SEATCHOICE["AISLE"] = "aisle";
+    SEATCHOICE[SEATCHOICE["MIDDLE"] = 3] = "MIDDLE";
+    SEATCHOICE[SEATCHOICE["WINDOW"] = 4] = "WINDOW";
+    SEATCHOICE[SEATCHOICE["FOURTH"] = 5] = "FOURTH";
+})(SEATCHOICE || (SEATCHOICE = {}));
+var seat = SEATCHOICE.AISLE;
+console.log(seat);
---
+++ TS/enum.ts
@@ -0,0 +1,11 @@
+enum SEATCHOICE{
+    AISLE = 'aisle',
+    MIDDLE=3,
+    WINDOW,
+    FOURTH
+} 
+
+const seat = SEATCHOICE.AISLE;
+console.log(seat)
+
+export{}
