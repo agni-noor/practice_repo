e8af5ccb388bcece5c1846ff2403914990f4caf3
agni-noor <agniotulonio.noor@optimizely.com>
2024-12-26 15:20:11 +0600

    interface

--- TS/project/src/index.ts
+++ TS/project/src/index.ts
@@ -1,11 +1,31 @@
 class User{
+
+    protected course_count = 1
     city:string = "Dhaka"
     constructor(
         public email:string,
         public name:string
     )
     {}
+
+    get CourseCount():number{
+        return this.course_count
+    }
+    set CourseCount(courseCount:number){
+        if(courseCount<1){
+            throw new Error("course count should be more than 1")
+        }
+        this.course_count = courseCount
+    }
+}
+class Subuser extends User{
+    isFamily:boolean = true
+    changeCourseCount(){
+        this.course_count = 4
+    }
 }
 
 const agni = new User("agni@gmail.com", "agni")
 console.log(agni)
+agni.CourseCount=3
+console.log(agni.CourseCount)
